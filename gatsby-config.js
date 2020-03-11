37a4db3928ee3667c37bb9d17a735ecd74086f8f
ArjanvHugten <arjan.van.hugten@outlook.com>
2020-03-11 15:56:34 +0100

    Changed to gatsby setup

---
+++ gatsby-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  siteMetadata: {
+    title: `Arjan van Hugten`,
+    description: `This is the blog of Arjan van Hugten`,
+    author: `Arjan van Hugten`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Arjan van Hugten blog`,
+        short_name: `ArjanvanHugten`,
+        start_url: `/`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
+        display: `minimal-ui`,
+      },
+    },
+  ],
+}
