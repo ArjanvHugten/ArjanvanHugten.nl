bc9e968abe3e31b9958a0b31d66e6333beda7e86
ArjanvHugten <arjan.van.hugten@outlook.com>
2020-03-14 22:52:18 +0100

    Fix for contact me form

--- src/components/general/contact-form.js
+++ src/components/general/contact-form.js
@@ -5,12 +5,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import '../../utils/fontawesome'
 
 export default () => (
-    <form className="contact-me-form" method="post" action="/contact-me/success" netlify-honeypot="bot-field" data-netlify="true">
+    <form className="contact-me-form" name="contact-me-form" method="post" action="/contact-me/success" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="contact-me-form" />
         <input type="hidden" name="bot-field" />
         <div className="field">
             <label htmlFor="name" className="label">Name</label>
             <div className="control has-icons-left">
-                <input id="name" required className="input" type="text" placeholder="Name"/>
+                <input id="name" name="name" className="input" type="text" placeholder="Name" />
                 <span className="icon is-small is-left">
                     <FontAwesomeIcon icon="align-left" />
                 </span>
@@ -20,7 +21,7 @@ export default () => (
         <div className="field">
             <label htmlFor="email" className="label">Email</label>
             <div className="control has-icons-left">
-                <input id="email" required className="input" type="email" placeholder="Email"/>
+                <input id="email" name="email" className="input" type="email" placeholder="Email" />
                 <span className="icon is-small is-left">
                     <FontAwesomeIcon icon="envelope" />
                 </span>
@@ -30,13 +31,13 @@ export default () => (
         <div className="field">
             <label htmlFor="message" className="label">Message</label>
             <div className="control">
-                <textarea id="message" required className="textarea" placeholder="Message"></textarea>
+                <textarea id="message" name="message" className="textarea" placeholder="Message" required></textarea>
             </div>
         </div>
 
         <div className="field is-grouped">
             <div className="control">
-                <button type="submit" className="button is-primary">Submit</button>
+                <input type="submit" className="button is-primary" value="Submit" />
             </div>
             <div className="control">
                 <Link to="/contact-me/" className="button is-light">Cancel</Link>
