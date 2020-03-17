b543f78d4d7def2e976f09061127fc07101b2d42
ArjanvHugten <arjan.van.hugten@outlook.com>
2020-03-17 19:43:10 +0100

    Created PWA and contact form clear fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,5 +76,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-force-trailing-slashes`,
+    `gatsby-plugin-offline`,
   ],
 }
--- src/components/general/contact-form.js
+++ src/components/general/contact-form.js
@@ -1,47 +1,51 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useRef } from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import '../../utils/fontawesome'
 
-export default () => (
-    <form className="contact-me-form" name="contact-me-form" method="post" action="/contact-me/success" data-netlify="true" data-netlify-honeypot="bot-field">
-        <input type="hidden" name="form-name" value="contact-me-form" />
-        <input type="hidden" name="bot-field" />
-        <div className="field">
-            <label htmlFor="name" className="label">Name</label>
-            <div className="control has-icons-left">
-                <input id="name" name="name" className="input" type="text" placeholder="Name" />
-                <span className="icon is-small is-left">
-                    <FontAwesomeIcon icon="align-left" />
-                </span>
-            </div>
-        </div>
+export default () => {
+    let contactFormElement = useRef(null);
+    function clearForm() { contactFormElement.current.reset() }
 
-        <div className="field">
-            <label htmlFor="email" className="label">Email</label>
-            <div className="control has-icons-left">
-                <input id="email" name="email" className="input" type="email" placeholder="Email" />
-                <span className="icon is-small is-left">
-                    <FontAwesomeIcon icon="envelope" />
-                </span>
+    return (
+        <form ref={contactFormElement} className="contact-me-form" name="contact-me-form" method="post" action="/contact-me/success" data-netlify="true" data-netlify-honeypot="bot-field">
+            <input type="hidden" name="form-name" value="contact-me-form" />
+            <input type="hidden" name="bot-field" />
+            <div className="field">
+                <label htmlFor="name" className="label">Name</label>
+                <div className="control has-icons-left">
+                    <input id="name" name="name" className="input" type="text" placeholder="Name" />
+                    <span className="icon is-small is-left">
+                        <FontAwesomeIcon icon="align-left" />
+                    </span>
+                </div>
             </div>
-        </div>
 
-        <div className="field">
-            <label htmlFor="message" className="label">Message</label>
-            <div className="control">
-                <textarea id="message" name="message" className="textarea" placeholder="Message" required></textarea>
+            <div className="field">
+                <label htmlFor="email" className="label">Email</label>
+                <div className="control has-icons-left">
+                    <input id="email" name="email" className="input" type="email" placeholder="Email" />
+                    <span className="icon is-small is-left">
+                        <FontAwesomeIcon icon="envelope" />
+                    </span>
+                </div>
             </div>
-        </div>
 
-        <div className="field is-grouped">
-            <div className="control">
-                <input type="submit" className="button is-primary" value="Submit" />
+            <div className="field">
+                <label htmlFor="message" className="label">Message</label>
+                <div className="control">
+                    <textarea id="message" name="message" className="textarea" placeholder="Message" required></textarea>
+                </div>
             </div>
-            <div className="control">
-                <Link to="/contact-me/" className="button is-light">Cancel</Link>
+
+            <div className="field is-grouped">
+                <div className="control">
+                    <input type="submit" className="button is-primary" value="Submit" />
+                </div>
+                <div className="control">
+                    <button className="button is-light" onClick={clearForm}>Cancel</button>
+                </div>
             </div>
-        </div>
-    </form>
-)
+        </form>
+    )
+}
